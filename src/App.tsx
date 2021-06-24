f18da9859434a2c22f3cede42065cbcb444984fa
joaorpereira <jprp17@live.com>
2021-06-24 01:29:08 -0300

    chore: setup eslint and prettier

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router } from "react-router-dom";
+// import { Provider } from "react-redux";
+
+function App() {
+  return (
+    // <Provider store={null}>
+    <Router></Router>
+    // </Provider>
+  );
+}
+
+export default App;
