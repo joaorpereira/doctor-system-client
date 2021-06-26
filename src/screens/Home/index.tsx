d3d6490ff8b03828fa7098fd27107ed76ba8ae31
joaorpereira <jprp17@live.com>
2021-06-26 20:23:21 -0300

    feature: insert react-big-calendar into home page

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,51 @@
-import React, { ReactElement, useEffect } from "react";
-import { useAppDispatch } from "../../hooks/hooks";
-import { getCompanies } from "../../store/ducks/companiesSlice";
+import React, { ReactElement } from "react";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import * as S from "./styled";
+// import { useAppDispatch } from "../../hooks/hooks";
+// import { getCompanies } from "../../store/ducks/companiesSlice";
+
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import { format, parse, startOfWeek, getDay, addMinutes } from "date-fns";
+import pt from "date-fns/locale/pt-BR";
+
+const locales = {
+  "pt-BR": pt,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
 
 const HomePage: React.FC = (): ReactElement => {
-  const dispatch = useAppDispatch();
+  // const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    dispatch(getCompanies());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(getCompanies());
+  // }, [dispatch]);
+
+  const date = new Date();
 
   return (
-    <div>
+    <S.ScheduleSection>
       <h1>Agendamentos</h1>
-    </div>
+      <Calendar
+        localizer={localizer}
+        events={[
+          {
+            title: "Evento teste",
+            start: date,
+            end: addMinutes(date, 30),
+          },
+        ]}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 650 }}
+      />
+    </S.ScheduleSection>
   );
 };
 
---
+++ src/screens/Home/styled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ScheduleSection = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-family: "Poppins", sans-serif;
+    margin-bottom: 35px;
+    font-size: 2.4rem;
+    font-weight: 900;
+  }
+`;
