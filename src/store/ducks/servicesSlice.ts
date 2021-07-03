2b4ab27d7c718fed27cf8ff91d04f10ae4ab06ec
joaorpereira <jprp17@live.com>
2021-07-03 20:12:33 -0300

    feature: create actions, reducers, slices and sagas for services

--- src/store/ducks/servicesSlice.ts
+++ src/store/ducks/servicesSlice.ts
@@ -1,8 +1,15 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+export type File = {
+  reference_id: string;
+  model: string;
+  folder: string;
+  created_at: string;
+};
+
 export type Service = {
   status?: string;
-  created_at?: string;
+  created_at?: Date;
   _id?: string;
   company_id?: string;
   title?: string;
@@ -10,23 +17,30 @@ export type Service = {
   service_duration?: string;
   service_recurrence?: number;
   description?: string;
+  files?: File[];
 };
 
-export type ServiceEntry = {
-  services: Service;
-  file: string[];
+export type ServicesOptions = {
+  label: string;
+  value: string;
+};
+
+export type ServicePayload = {
+  service: Service;
 };
 
 interface ServicesSliceState {
-  services: ServiceEntry[];
-  service?: Record<string, never>;
+  services: ServicePayload[];
+  service?: Service;
   type: string;
   loading?: boolean;
+  servicesOptions: ServicesOptions[];
 }
 
 const initialState: ServicesSliceState = {
   services: [],
   service: {},
+  servicesOptions: [],
   type: "",
   loading: false,
 };
@@ -35,17 +49,72 @@ const servicesSlice = createSlice({
   name: "services",
   initialState: initialState,
   reducers: {
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    getService(state, action: PayloadAction<any>) {
+      return { ...state, loading: true };
+    },
     getServices(state, action: PayloadAction<any>) {
       return { ...state, loading: true };
+    },
+    getFilteredServices(state, action: PayloadAction<any>) {
+      return { ...state, loading: true };
+    },
+    createService(state, action: PayloadAction<any>) {
+      return { ...state, loading: true };
+    },
+    updateService(state, action: PayloadAction<any>) {
+      return { ...state, loading: true };
+    },
+    removeService(state, action: PayloadAction<any>) {
+      return { ...state, loading: true };
+    },
+    setService: (state, action: PayloadAction<any>) => {
+      const service = action.payload.service;
+      const type = action.payload.type;
+      return { ...state, service, type: type, loading: false };
     },
     setServicesSuccess(state, action: PayloadAction<any>) {
       const services = action.payload;
       return { ...state, ...services, loading: false };
     },
+    setFilteredServicesSuccess(state, action: PayloadAction<any>) {
+      const servicesOptions = action.payload;
+      return { ...state, ...servicesOptions, loading: false };
+    },
+    createServicesSuccess(state, action: PayloadAction<any>) {
+      const service = action.payload.service;
+      state.services = [...state.services, service];
+      state.loading = false;
+    },
+    updateServiceSuccess(state, action: PayloadAction<any>) {
+      const { service } = action.payload;
+      const serviceIndex = state.services.findIndex(
+        (item: any) => item._id === service._id
+      );
+      state.services[serviceIndex] = service;
+      state.loading = false;
+    },
+    removeServiceSuccess: (state, action: PayloadAction<any>) => {
+      state.services = state.services.filter(
+        (item: any) => item._id !== action.payload.id
+      );
+      state.loading = false;
+    },
   },
 });
 
-export const { getServices, setServicesSuccess } = servicesSlice.actions;
+export const {
+  getService,
+  getServices,
+  getFilteredServices,
+  createService,
+  updateService,
+  removeService,
+  setService,
+  setServicesSuccess,
+  setFilteredServicesSuccess,
+  createServicesSuccess,
+  updateServiceSuccess,
+  removeServiceSuccess,
+} = servicesSlice.actions;
 
 export default servicesSlice.reducer;
--- src/store/sagas/servicesSaga.ts
+++ src/store/sagas/servicesSaga.ts
@@ -4,6 +4,14 @@ import {
   Service,
   getServices,
   setServicesSuccess,
+  setFilteredServicesSuccess,
+  getFilteredServices,
+  updateServiceSuccess,
+  removeServiceSuccess,
+  createServicesSuccess,
+  removeService,
+  updateService,
+  createService,
 } from "../ducks/servicesSlice";
 import { ResponseGenerator } from "../common/types";
 
@@ -11,6 +19,7 @@ type ServicesProps = {
   payload: {
     id?: string;
     service?: Service;
+    status?: string;
   };
   type: string;
 };
@@ -27,4 +36,57 @@ function* handleGetServices({ payload }: ServicesProps) {
   }
 }
 
-export default all([takeLatest(getServices.type, handleGetServices)]);
+function* handleFilterServices({ payload }: ServicesProps) {
+  try {
+    const { data }: ResponseGenerator = yield call(
+      api.get,
+      `/service/filter/${payload.id}`
+    );
+    yield put(setFilteredServicesSuccess({ servicesOptions: data.services }));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function* handleCreateServices({ payload }: ServicesProps) {
+  try {
+    const { data }: ResponseGenerator = yield call(api.post, "/service", {
+      service: payload.service,
+    });
+    yield put(createServicesSuccess({ services: data.services }));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function* handleUpdateServices({ payload }: ServicesProps) {
+  try {
+    const { data }: ResponseGenerator = yield call(
+      api.put,
+      `/service/${payload.id}`
+    );
+    yield put(updateServiceSuccess({ services: data.services }));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function* handleRemoveServices({ payload }: ServicesProps) {
+  try {
+    const { data }: ResponseGenerator = yield call(
+      api.delete,
+      `/service/${payload.id}/${payload.status}`
+    );
+    yield put(removeServiceSuccess({ services: data.services }));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default all([
+  takeLatest(getServices.type, handleGetServices),
+  takeLatest(getFilteredServices.type, handleFilterServices),
+  takeLatest(createService.type, handleCreateServices),
+  takeLatest(updateService.type, handleUpdateServices),
+  takeLatest(removeService.type, handleRemoveServices),
+]);
