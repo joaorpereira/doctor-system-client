7d5f195592358e1775ea628dab4232a8a2313701
joaorpereira <jprp17@live.com>
2021-07-05 19:29:53 -0300

    feature: create new component imageItem

---
+++ src/components/ImageItem/index.tsx
@@ -0,0 +1,35 @@
+import * as S from "./styled";
+
+import { MdClear } from "react-icons/md";
+
+type ImageProps = {
+  created_at: string;
+  folder: string;
+  model: string;
+  reference_id: string;
+  updated_at: string;
+  __v: number;
+  _id: string;
+};
+
+type Props = {
+  images: ImageProps[];
+  handleRemoveImage: () => void;
+};
+
+export const ImageItem = ({ images, handleRemoveImage }: Props) => {
+  const BUCKET_URL = process.env.REACT_APP_BUCKET_URL;
+  return (
+    <>
+      {images &&
+        images.map((image) => (
+          <S.Box key={image._id}>
+            <S.Image src={`${BUCKET_URL}${image.folder}`} alt={image.folder} />
+            <S.RemoveButton onClick={() => handleRemoveImage()}>
+              <MdClear size={23} color="#c70000" />
+            </S.RemoveButton>
+          </S.Box>
+        ))}
+    </>
+  );
+};
---
+++ src/components/ImageItem/styled.ts
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import { colors } from "../../styles/variables";
+
+export const Box = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  height: 120px;
+  width: 120px;
+  border-radius: 4px;
+  outline: none;
+`;
+
+export const Image = styled.img`
+  object-fit: cover;
+  height: 120px;
+  width: 120px;
+  border-radius: 4px;
+`;
+
+export const RemoveButton = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 10;
+  border: none;
+  outline: none;
+  margin-right: -5px;
+  background-color: transparent;
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const AddButton = styled.div`
+  height: 120px;
+  width: 120px;
+  border: none;
+  border-radius: 4px;
+  outline: none;
+  background-color: ${colors.blu};
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
