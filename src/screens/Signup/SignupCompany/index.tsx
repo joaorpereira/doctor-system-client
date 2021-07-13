96286ebefd306445d6502d74d9b95635e11b4d01
joaorpereira <jprp17@live.com>
2021-07-12 21:50:27 -0300

    feature: add company signup page

---
+++ src/screens/Signup/SignupCompany/index.tsx
@@ -0,0 +1,337 @@
+import { Controller, useForm } from "react-hook-form";
+import ReactSelect from "react-select";
+import * as S from "./styled";
+
+import { Spinner } from "../../../components/Spinner";
+import InputComponent from "../../../components/Input";
+import { accountsTypesOptions, OptionType } from "../../../utils/globalTypes";
+import { reactSelectedStyleSigupPage } from "../../../styles/global";
+import { countryList } from "../../../utils/countries";
+import { states } from "../../../utils/states";
+import { bankList } from "../../../utils/banksList";
+
+const SignupCompany = ({
+  onSubmit,
+  handlePhoneMask,
+  handleCpfOrCnpjMask,
+  handleCountryChange,
+  handleCepMask,
+  handleGetAPIAdressInformation,
+  handleStateChange,
+  handlePicture,
+  handleBackgroundImage,
+  handleAccountType,
+  handleBankCode,
+  fetchCep,
+  streetValue,
+  stateValue,
+  cityValue,
+  cpfValue,
+  bankInfoPage,
+  setBankInfoPage,
+}: any) => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { isSubmitting },
+  } = useForm({});
+
+  return (
+    <S.Form onSubmit={handleSubmit(onSubmit)}>
+      {!bankInfoPage ? (
+        <>
+          <S.InputImageContainer>
+            <S.InputImage
+              type="file"
+              name="myImage"
+              accept="image/png, image/gif, image/jpeg"
+              onChange={(e) => handlePicture(e)}
+            />
+            <S.InputImage
+              type="file"
+              name="myImage"
+              accept="image/png, image/gif, image/jpeg"
+              onChange={(e) => handleBackgroundImage(e)}
+            />
+          </S.InputImageContainer>
+          <h1>Dados da Empresa</h1>
+          <S.FormSection>
+            <InputComponent width="295px" secondary name="name" label="Nome:">
+              <S.Input type="text" {...register("name")} />
+            </InputComponent>
+            <InputComponent width="295px" secondary name="email" label="Email:">
+              <S.Input type="email" {...register("email")} />
+            </InputComponent>
+            <InputComponent
+              width="180px"
+              secondary
+              name="phone_number"
+              label="Telefone:"
+            >
+              <S.Input
+                type="tel"
+                maxLength={12}
+                placeholder="+55 99999-9999"
+                {...register("phone_number")}
+                onChange={(e) => handlePhoneMask(e)}
+              />
+            </InputComponent>
+
+            <InputComponent
+              width="200px"
+              secondary
+              name="password"
+              label="Senha:"
+            >
+              <S.Input type="password" {...register("password")} />
+            </InputComponent>
+          </S.FormSection>
+          <h1>Endereço</h1>
+          <S.FormSection>
+            <InputComponent secondary name="address.country" label="País:">
+              <Controller
+                name="address.country"
+                control={control}
+                render={({ field }) => (
+                  <ReactSelect
+                    {...field}
+                    styles={{
+                      control: (base) => ({
+                        ...base,
+                        ...reactSelectedStyleSigupPage,
+                        width: "177px !important",
+                      }),
+                    }}
+                    value={countryList.filter(
+                      (option: OptionType) => option.value === "BR"
+                    )}
+                    options={countryList}
+                    onChange={(e) => handleCountryChange(e as OptionType)}
+                  />
+                )}
+              />
+            </InputComponent>
+            <InputComponent
+              width="200px"
+              secondary
+              name="address.cep"
+              label="CEP:"
+            >
+              <S.Input
+                type="text"
+                maxLength={9}
+                {...register("address.cep")}
+                placeholder="99999-999"
+                onChange={(e) => handleCepMask(e)}
+                onBlur={(e) => handleGetAPIAdressInformation(e)}
+              />
+              {fetchCep && <Spinner loading size="30px" />}
+            </InputComponent>
+            <InputComponent secondary name="address.state" label="Estado:">
+              <Controller
+                name="address.state"
+                control={control}
+                render={({ field }) => (
+                  <ReactSelect
+                    {...field}
+                    value={states.filter(
+                      (option: OptionType) => option.value === stateValue
+                    )}
+                    styles={{
+                      control: (base) => ({
+                        ...base,
+                        ...reactSelectedStyleSigupPage,
+                        width: "177px !important",
+                      }),
+                    }}
+                    options={states}
+                    onChange={(e) => handleStateChange(e as OptionType)}
+                  />
+                )}
+              />
+            </InputComponent>
+            <InputComponent
+              width="201px"
+              secondary
+              name="address.city"
+              label="Cidade:"
+            >
+              <S.Input
+                type="text"
+                defaultValue={cityValue}
+                {...register("address.city")}
+              />
+            </InputComponent>
+            <InputComponent
+              width="256px"
+              secondary
+              name="address.street"
+              label="Rua:"
+            >
+              <S.Input
+                type="text"
+                defaultValue={streetValue}
+                {...register("address.street")}
+              />
+            </InputComponent>
+            <InputComponent
+              width="80px"
+              secondary
+              name="address.number"
+              label="Número:"
+            >
+              <S.Input type="text" {...register("address.number")} />
+            </InputComponent>
+            <InputComponent
+              width="150px"
+              secondary
+              name="geolocation.coordinates.x"
+              label="CordX:"
+            >
+              <S.Input
+                type="number"
+                {...register("geolocation.coordinates.x")}
+              />
+            </InputComponent>
+            <InputComponent
+              width="150px"
+              secondary
+              name="geolocation.coordinates.y"
+              label="CordY:"
+            >
+              <S.Input
+                type="number"
+                {...register("geolocation.coordinates.y")}
+              />
+            </InputComponent>
+          </S.FormSection>
+        </>
+      ) : (
+        <>
+          <h1>Dados Bancários</h1>
+          <S.FormSection>
+            <InputComponent
+              width="285px"
+              secondary
+              name="bank_account.acc_user_name"
+              label="Titular:"
+            >
+              <S.Input
+                type="text"
+                {...register("bank_account.acc_user_name")}
+              />
+            </InputComponent>
+            <InputComponent
+              width="285px"
+              secondary
+              name="bank_account.cpf_or_cnpj"
+              label="CPF/CNPJ:"
+            >
+              <S.Input
+                type="text"
+                value={cpfValue}
+                {...register("bank_account.cpf_or_cnpj")}
+                onChange={(e) => handleCpfOrCnpjMask(e)}
+              />
+            </InputComponent>
+            <InputComponent
+              secondary
+              name="bank_account.bank_code"
+              label="Banco:"
+              width="218px"
+            >
+              <Controller
+                name="bank_account.bank_code"
+                control={control}
+                render={({ field }) => (
+                  <ReactSelect
+                    {...field}
+                    styles={{
+                      control: (base) => ({
+                        ...base,
+                        ...reactSelectedStyleSigupPage,
+                      }),
+                    }}
+                    placeHolder=""
+                    options={bankList}
+                    onChange={(e) => handleBankCode(e as OptionType)}
+                  />
+                )}
+              />
+            </InputComponent>
+            <InputComponent
+              width="218px"
+              secondary
+              name="bank_account.acc_type"
+              label="Tipo da Conta:"
+            >
+              <Controller
+                name="bank_account.acc_type"
+                control={control}
+                render={({ field }) => (
+                  <ReactSelect
+                    {...field}
+                    styles={{
+                      control: (base) => ({
+                        ...base,
+                        ...reactSelectedStyleSigupPage,
+                      }),
+                    }}
+                    placeHolder=""
+                    options={accountsTypesOptions}
+                    onChange={(e) => handleAccountType(e as OptionType)}
+                  />
+                )}
+              />
+            </InputComponent>
+            <InputComponent
+              width="218px"
+              secondary
+              name="bank_account.bank_agency"
+              label="Agência:"
+            >
+              <S.Input type="text" {...register("bank_account.bank_agency")} />
+            </InputComponent>
+            <InputComponent
+              width="218px"
+              secondary
+              name="bank_account.acc_number"
+              label="Número:"
+            >
+              <S.Input type="text" {...register("bank_account.acc_number")} />
+            </InputComponent>
+            <InputComponent
+              width="100px"
+              secondary
+              name="bank_account.verify_digit"
+              label="Dígito:"
+            >
+              <S.Input type="text" {...register("bank_account.verify_digit")} />
+            </InputComponent>
+          </S.FormSection>
+        </>
+      )}
+      <S.BtnDiv>
+        {bankInfoPage ? (
+          <S.Button disabled={isSubmitting} type="submit">
+            {!isSubmitting ? (
+              <p>Enviar</p>
+            ) : (
+              <Spinner
+                size="35px"
+                style={{ position: "absolute", top: 35, left: 195 }}
+              />
+            )}
+          </S.Button>
+        ) : (
+          <S.SpanButton onClick={() => setBankInfoPage(true)}>
+            <p>Próximo</p>
+          </S.SpanButton>
+        )}
+      </S.BtnDiv>
+    </S.Form>
+  );
+};
+
+export default SignupCompany;
---
+++ src/screens/Signup/SignupCompany/styled.ts
@@ -0,0 +1,249 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { colors } from "../../../styles/variables";
+
+type FlexProps = {
+  direction?: string;
+  height?: string;
+};
+
+type SpanLinkProps = {
+  align?: string;
+  margintop?: string;
+  fontSize?: string;
+};
+
+type FormSectionProps = {
+  align?: string;
+  justify?: string;
+};
+
+type InputProps = {
+  width?: string;
+};
+
+export const LoginSection = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media only screen and (min-width: 350px) and (max-width: 800px) {
+    padding: 50px 50px;
+  }
+`;
+
+export const FlexSection = styled.div<FlexProps>`
+  @media only screen and (min-width: 350px) and (max-width: 800px) {
+    :nth-child(3) {
+      display: none;
+    }
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    padding: 0px;
+  }
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+
+  background-color: ${({ color }) =>
+    color ? color : `${colors.backgroundcolor}`};
+  flex-direction: ${({ direction }) => (direction ? direction : "row")};
+  padding: 0px 80px;
+`;
+
+export const Label = styled.label`
+  font-size: 1rem;
+  color: ${colors.text};
+  margin-bottom: 5px;
+`;
+
+export const Input = styled.input<InputProps>`
+  border: 1px solid ${colors.text};
+  outline: none;
+  padding: 10px;
+  border-radius: 4px;
+  transition: border 0.15s ease-out;
+  height: 46px;
+  background-color: #fff;
+  :focus {
+    border: 2px solid ${colors.text};
+  }
+
+  &::placeholder {
+    font-size: 0.7rem;
+    color: #a9a9a9;
+    background-color: #fff;
+  }
+`;
+
+export const Image = styled.img<FlexProps>`
+  height: ${({ height }) => (height ? height : "75%")};
+  margin-bottom: 30px;
+  @media only screen and (min-width: 350px) and (max-width: 800px) {
+    height: 120px;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin: 0 auto;
+
+  h1 {
+    margin-bottom: 20px;
+    font-size: 1.8rem;
+  }
+
+  @media only screen and (min-width: 350px) and (max-width: 800px) {
+    height: 100%;
+    align-items: center;
+    h1 {
+      margin: 15px 0px;
+      font-size: 1.25rem;
+      text-transform: uppercase;
+    }
+  }
+`;
+
+export const Button = styled.button`
+  position: relative;
+  background-color: ${colors.text};
+  color: #fff;
+  font-weight: 600;
+  text-transform: uppercase;
+  height: 58px;
+  width: 375px;
+  border: none;
+  border-radius: 4px;
+
+  margin-top: 30px;
+
+  transition: 0.2s filter;
+  margin: 0 auto;
+
+  font-size: 1.4rem;
+  text-transform: uppercase;
+  letter-spacing: 0.1rem;
+
+  transition: 0.2s filter;
+
+  &:not(:disabled):hover {
+    filter: brightness(2);
+  }
+`;
+
+export const SpanButton = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${colors.text};
+  color: #fff;
+  font-weight: 600;
+  text-transform: uppercase;
+  height: 58px;
+  width: 375px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 30px;
+
+  transition: 0.2s filter;
+  margin: 0 auto;
+
+  font-size: 1.35rem;
+  text-transform: uppercase;
+  letter-spacing: 0.1rem;
+
+  transition: 0.2s filter;
+
+  &:not(:disabled):hover {
+    filter: brightness(2);
+  }
+`;
+
+export const BtnDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin-top: 30px;
+
+  @media only screen and (min-width: 350px) and (max-width: 800px) {
+    margin: 0px;
+  }
+`;
+
+export const SpanLink = styled(Link)<SpanLinkProps>`
+  text-align: ${({ align }) => (align ? align : "right")};
+  margin-top: ${({ margintop }) => (margintop ? margintop : "10px")};
+  font-size: ${({ fontSize }) => (fontSize ? fontSize : "0.85rem")};
+  font-weight: bold;
+`;
+
+export const BtnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 350px;
+`;
+
+export const BtnReturn = styled.button`
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin: 20px 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: transparent;
+  color: ${colors.text};
+  font-weight: bold;
+  text-transform: uppercase;
+  border: none;
+  transition: 0.2s filter;
+  font-size: 1.2rem;
+  text-transform: uppercase;
+  letter-spacing: 0.1rem;
+  transition: 0.2s filter;
+
+  :hover {
+    filter: brightness(2);
+  }
+
+  @media only screen and (min-width: 350px) and (max-width: 800px) {
+    p {
+      display: none;
+    }
+  }
+`;
+
+export const FormSection = styled.div<FormSectionProps>`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 15px;
+  align-items: ${({ align }) => (align ? align : "start")};
+  justify-content: ${({ justify }) => (justify ? justify : "start")};
+  margin-bottom: 40px;
+`;
+
+export const InputImage = styled.input`
+  height: 125px;
+  width: 125px;
+  border: 1px solid black;
+  border-radius: 50%;
+`;
+
+export const InputImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 30px;
+  gap: 50px;
+`;
