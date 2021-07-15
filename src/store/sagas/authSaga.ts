64b468a7821422d933ac2695361ce3706c5fd9e6
joaorpereira <jprp17@live.com>
2021-07-14 21:50:24 -0300

    feature: create login and logout auth saga

---
+++ src/store/sagas/authSaga.ts
@@ -0,0 +1,48 @@
+import api from "../../services/api";
+import { all, call, put, takeLatest } from "redux-saga/effects";
+import {
+  requestLogin,
+  requestLogout,
+  requestLoginSuccess,
+  requestLogoutSuccess,
+} from "../ducks/authSlice";
+import { ResponseGenerator } from "../common/types";
+import { key } from "../../hooks/useLocalStorage";
+
+type PropsPayload = {
+  type: string;
+  payload: {
+    email: string;
+    password: string;
+    typeUser: "client" | "worker" | "company";
+  };
+};
+
+function* handleLogin({ payload }: PropsPayload) {
+  try {
+    const { data }: ResponseGenerator = yield call(
+      api.post,
+      `/${payload.typeUser}/login`,
+      { email: payload.email, password: payload.password }
+    );
+    yield put(requestLoginSuccess({ user: data.user, token: data.token }));
+  } catch (error) {
+    localStorage.removeItem(key);
+    console.log(error);
+  }
+}
+
+function* handleLogout() {
+  try {
+    localStorage.removeItem(key);
+    yield put(requestLogoutSuccess({}));
+  } catch (error) {
+    localStorage.removeItem(key);
+    console.log(error);
+  }
+}
+
+export default all([
+  takeLatest(requestLogin.type, handleLogin),
+  takeLatest(requestLogout.type, handleLogout),
+]);
