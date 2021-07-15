94caa911d129b9410bf7b092f8ebb02bcd344dd3
joaorpereira <jprp17@live.com>
2021-07-15 13:38:48 -0300

    refactor: create index file for home custom hooks to make it easier to import the component

--- src/screens/Home/hooks/useFormatScheduleData.ts
+++ src/screens/Home/hooks/useFormatScheduleData.ts
@@ -1,34 +1,19 @@
 import { addMinutes } from "date-fns";
 import { Dispatch, SetStateAction, useCallback } from "react";
+import {
+  FormatedSchedule,
+  Schedule,
+} from "../../../store/ducks/schedulesSlice";
 import { convertHourToMinutes } from "../../../utils/date";
 
-export type IScheduleData = {
-  client_id: { _id: string; name: string };
-  schedule_date: string;
-  service_id: { _id: string; title: string; service_duration: string };
-  worker_id: { _id: string; name: string };
-  company_id: string;
-  created_at: string;
-  transaction_id: string;
-  updated_at: string;
-  __v: string;
-  _id: string;
-};
-
-export type IFormatedSchedule = {
-  title: string;
-  start: Date;
-  end: Date;
-};
-
 type ISetState = {
-  setScheduleData: Dispatch<SetStateAction<IFormatedSchedule[]>>;
+  setScheduleData: Dispatch<SetStateAction<FormatedSchedule[]>>;
 };
 
 function useFormatScheduleData({ setScheduleData }: ISetState) {
-  const formatScheduleData = useCallback((data: IScheduleData[]) => {
-    const response: IFormatedSchedule[] = data.map(
-      ({ service_id, client_id, worker_id, schedule_date }: IScheduleData) => ({
+  const formatScheduleData = useCallback((data: Schedule[]) => {
+    const response: FormatedSchedule[] = data.map(
+      ({ service_id, client_id, worker_id, schedule_date }: Schedule) => ({
         title: `${service_id.title} - ${client_id.name} - ${worker_id.name}`,
         start: new Date(schedule_date),
         end: addMinutes(
