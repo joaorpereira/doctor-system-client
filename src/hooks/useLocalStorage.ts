002157eea255ddbffe3fab110e0887aefedd87ac
joaorpereira <jprp17@live.com>
2021-07-14 21:47:27 -0300

    feature: create useLocalStorage custom hook

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const initialValue = null;
+export const key = "@doctor_system";
+
+export const useLocalStorage = () => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const setValue = (value: any) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return { storedValue, setValue };
+};
