ecf97fd1a9b257a1f87f8ab06890ef7d5ad3a9f9
joaorpereira <jprp17@live.com>
2021-08-01 16:40:57 -0300

    feature: create useOnSubmit custom hook for Hour Page

--- src/screens/Hours/hooks/useOnSubmit.ts
+++ src/screens/Hours/hooks/useOnSubmit.ts
@@ -1,54 +1,45 @@
+import { OptionTypeBase } from "react-select";
 import { useAppDispatch } from "../../../hooks";
-import {
-  Service,
-  updateService,
-  createService,
-} from "../../../store/ducks/servicesSlice";
+import { createHour, updateHour } from "../../../store/ducks/hoursSlice";
 import { actionsTypes } from "../../../utils";
+import { OptionType } from "../../../utils/types";
 
 type OnSubmitProps = {
-  type: string;
+  disponibleDays: OptionTypeBase | readonly OptionTypeBase[] | null | undefined;
+  company_id?: string;
+  disponibleWorkers: OptionType[];
+  disponibleServices: OptionType[];
+  startTime: OptionType | null;
+  endTime: OptionType | null;
+  type?: string;
   id?: string;
-  statusValue: string;
-  companyValue?: string;
-  durationValue: string;
-  images: never[];
 };
 
 const useOnSubmit = ({
+  disponibleDays,
+  company_id,
+  disponibleWorkers,
+  disponibleServices,
+  startTime,
+  endTime,
   id,
   type,
-  statusValue,
-  companyValue,
-  durationValue,
-  images,
-}: any) => {
+}: OnSubmitProps) => {
   const dispatch = useAppDispatch();
-  const onSubmit = (data: Service) => {
-    const newForm = {
-      ...data,
-      company_id: companyValue,
-      status: statusValue,
-      service_duration: durationValue,
-      price: Number(data.price),
-      service_recurrence: Number(data.service_recurrence),
+  const onSubmit = () => {
+    const data = {
+      company_id,
+      services: disponibleServices?.map((item: OptionType) => item.value),
+      workers: disponibleWorkers?.map((item: OptionType) => item.value),
+      days: disponibleDays?.map((item: OptionType) => item.value),
+      start_time: startTime?.value,
+      end_time: endTime?.value,
     };
 
     if (type === actionsTypes.UPDATE) {
-      dispatch(
-        updateService({
-          id: id,
-          service: { ...newForm },
-          files: images,
-        })
-      );
+      dispatch(createHour({ data }));
     } else if (type === actionsTypes.CREATE) {
-      dispatch(
-        createService({
-          service: { ...newForm },
-          files: images,
-        })
-      );
+      dispatch(updateHour({ id, data }));
     }
   };
 
